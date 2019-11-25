eb581e4682a870560510e261d4209794fac649f1
kindljiri <kindl_jiri@yahoo.com>
2019-11-25 15:40:34 +0100

    test_ssh update
    test_ssh can run command remotly.

--- test_ssh.ps1
+++ test_ssh.ps1
@@ -7,21 +7,25 @@
     File Name      : test_ssh.ps1  
     Author         : Jiri Kindl; kindl_jiri@yahoo.com
     Prerequisite   : PowerShell V2 over Vista and upper.
-    Version        : 20191107
+    Version        : 20191122
     Copyright 2019 - Jiri Kindl
 .LINK  
     
 .EXAMPLE  
-    .\test_ssh.ps1 -inputfile inputfile.txt -username YourUserName -password YourPassword
+    .\test_ssh.ps1 -inputfile inputfile.txt -username YourUserName -password YourPassword -commad uname
 #>
 
 #pars parametrs with param
 
-param([string]$inputfile = "default", [string]$username = "test", [string]$password = "ptest", [switch]$help)
+param([string]$inputfile = "default", [string]$username = "test", [string]$password = "ptest", [string]$command, [switch]$help)
 
 Function usage {
-  "test_ssh.ps1 -inputfile inputfile.txt -username YourUserName -password YourPassword"
+  "test_ssh.ps1 -inputfile inputfile.txt [-username YourUserName] [-password YourPassword] [-command] [-help]"
   "inputfile - file with IPs one per line"
+  "username - provide username if not provided, you'll be asked"
+  "password - provide password if not provided, you'll be asked, it's safer to not provide if poweshell keeps history"
+  "command - command to be executed remotely, if not provided it test just ability to login"
+  "help - print this help" 
   exit
 }
 
@@ -43,12 +47,18 @@ try {
       $SSHResult = New-SSHSession -computername $ip -Credential $credential -AcceptKey
       if ($SSHResult.Connected) {
         $SSHResultString = $SSHResult.Connected
+        if ($command) {
+          $SSHCommandResult = Invoke-SSHCommand -SessionId $SSHResult.SessionId -Command $command
+        }
         Remove-SSHSession -SessionId $SSHResult.SessionId
       }
       else {
         $SSHResultString = $Error[0]
       }      
     "$ip,$SSHResultString"
+    if ($command -and ($SSHResultString -eq "True")) {
+      $SSHCommandResult
+    }
   }
 }
 catch [System.Management.Automation.ItemNotFoundException] {
