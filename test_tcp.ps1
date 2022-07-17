c7881ab5209f499ebb8775c3dd0fd61b3cf440b7
kindljiri <kindl_jiri@yahoo.com>
2022-07-17 23:03:13 +0200

    Version 20220714
    Added -csvfile option to let test different port for different hosts, or multiple ports for one, whatever...

--- test_tcp.ps1
+++ test_tcp.ps1
@@ -7,7 +7,7 @@
     File Name      : test_tcp.ps1  
     Author         : Jiri Kindl; kindl_jiri@yahoo.com
     Prerequisite   : PowerShell V2 over Vista and upper.
-    Version        : 20191107
+    Version        : 20220714
     Copyright 2015 - Jiri Kindl    
 .LINK  
     
@@ -17,52 +17,122 @@
 
 #pars parametrs with param
 
-param([string]$inputfile, [int]$port, [switch]$help)
+param([string]$hostfile, [string]$csvfile, [int]$port=5895, [switch]$help)
 
 Function usage {
-  "test_tcp.ps1 -inputfile inputfile.txt [-port NUMBER]"
-  "inputfile - file with IPs one per line"
-  "port NUMBER - number of port to test, default is 5895"
+  "test_tcp.ps1 -hostfile testconnections.txt -csvfile testconnections.csv [-port NUMBER]"
+  "  -hostfile - file with IPs one per line"
+  "  -csvfile - file with deviceFQDN,port pairs one pair per line, (IPs can be used as well instead of FQDN)"
+  "  -port NUMBER - number of port to test, default is 5895"
   exit
 }
 
-if ((!$inputfile) -or (!$port) -or ($help)) {
-  usage
+function test-tcp {
+	param(
+		[Parameter()]
+		[string]$ip, [string]$port
+	)
+	
+	$ip=$ip.trim()
+  $t = New-Object Net.Sockets.TcpClient
+  try {
+    $t.Connect($ip,$port)
+    $TCPTestResult = $t.Connected
+  }
+  catch [System.Net.Sockets.SocketException] {
+    $TCPTestResult = $error[0].ToString()
+      if ($TCPTestResult -CMatch 'A connection attempt failed because the connected party did not properly respond after a period of time') {
+        $TCPTestResult = "Connection failed: Time out"
+      }
+      elseif ($TCPTestResult -CMatch 'No connection could be made because the target machine actively refused') {
+        $TCPTestResult = "Connection failed: Connection refused"
+      }
+      elseif ($TCPTestResult -CMatch 'No such host is known') {
+        $TCPTestResult = "Connection failed: DNS: No such host is known"
+      }
+  }
+  catch {
+    $TCPTestResult = "Connection failed" #FALSE
+  }
+  if ( $TCPTestResult -eq $true ) {
+    $TCPTestResult = 'Connection succeeded'
+  }
+  $TCPTestResult
+
 }
 
-try {
-  $ips=get-content $inputfile -ErrorAction Stop
+function hosttest {
+  param([string]$hostfile)
+
+  try {
+    $ips=get-content $hostfile -ErrorAction Stop
+
+    "Source Host,Source Ip,Destination Host,Destination Port(TCP),Result,Last Hop"
   
-  "IP,TCP $port"
-
-  Foreach ($ip in $ips) {
-    $ip=$ip.trim()
-    $t = New-Object Net.Sockets.TcpClient
-    try {
-      $t.Connect($ip,$port)
-      $TCPTestResult = $t.Connected
+    Foreach ($ip in $ips) {
+      $TCPTestResult = test-tcp $ip $port
+      "$source_host,$source_ip,$ip,$port,$TCPTestResult,"
     }
-    catch [System.Net.Sockets.SocketException] {
-        $TCPTestResult = $error[0].ToString()
-        if ($TCPTestResult -CMatch 'A connection attempt failed because the connected party did not properly respond after a period of time') {
-          $TCPTestResult = "Time out"
-        }
-        elseif ($TCPTestResult -CMatch 'No connection could be made because the target machine actively refused') {
-          $TCPTestResult = "Connection refused"
-        }
-      }
-      catch {
-        $TCPTestResult = "no connectivity" #FALSE
-      }
-
-    "$ip,$TCPTestResult"
+  }
+  catch [System.Management.Automation.ItemNotFoundException] {
+    "No such file: $hostfile"
+    ""
+    usage
+  }
+  catch {
+    $Error[0]
   }
 }
-catch [System.Management.Automation.ItemNotFoundException] {
-  "No such file"
-  ""
+
+function csvtest {
+  param([string]$csvfile)
+
+  try {
+    $connections=get-content $csvfile -ErrorAction Stop
+    
+    "Source Host,Source Ip,Destination Host,Destination Port(TCP),Result,Last Hop"
+  
+    Foreach ($connection in $connections) {
+      ($ip,$port) = $connection -split ','
+      $TCPTestResult = test-tcp $ip $port
+      "$source_host,$source_ip,$ip,$port,$TCPTestResult,"
+    }
+  }
+  catch [System.Management.Automation.ItemNotFoundException] {
+    "No such file: $csvfile"
+    ""
+    usage
+  }
+  catch {
+    $Error[0]
+  }
+}
+
+###############################################################################
+#
+# MAIN PROGRAM
+#
+##############################################################################
+
+
+if (((!$hostfile) -and (!$csvfile)) -or (!$port) -or ($help)) {
   usage
 }
-catch {
-  $Error[0]
+
+$source_host = hostname
+$source_ip = ((Get-NetIPAddress -AddressFamily IPV4).IPAddress | Where-Object { $_ -ne '127.0.0.1' }) -join ';'
+$inputfile_base = ($inputfile -split '\\')[-1] 
+$inputfile_base = ($inputfile_base -split "\.")[0]
+$troubleshootfile="troubleshoot_"+ $inputfile_base +".txt"
+
+
+if ($hostfile) {
+  hosttest $hostfile
 }
+elseif ($csvfile) {
+  csvtest $csvfile
+}
+
+#VERSION HISTORY:
+#Version,Comment
+#20220714,ADDED: CSV input for connection testing, and give it same output and CSV Header as its linux version
